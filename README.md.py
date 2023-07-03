f11b4117f4518b245d1505afc59443ea926a48d4
Sabrina240597 <sabrin.bonomo@gmail.com>
2023-07-03 19:13:41 -0300

    agregado de temperatua y adivinador de numero

--- README.md.py
+++ README.md.py
@@ -11,7 +11,7 @@ suma = int(numero1) + int(numero2)
 #Imprimir la suma
 print("la suma de", numero1 ,"y", numero2 , "es", suma )
 
-######################################################################
+######################################################################################
 
 #creando una calculadora
 
@@ -34,4 +34,75 @@ else:
     resultado = "Operación inválida"
 
 print("su resultado es...", resultado)
-#####################################################################
+#######################################################################################
+
+#contador de numeros pares:
+
+#Inicializar una variable para contar los números pares
+contador = 0
+
+#Inicializar una variable para almacenar el número actual
+numero = 0
+
+#hasta que se encuentren los primeros 10 números pares
+while contador < 10:
+    #Verificar si el número actual es par
+    if numero % 2 == 0:
+        #Imprimir el número par
+        print(numero)
+        #Incrementar el contador de números pares
+        contador += 1
+    #Incrementar el número actual
+    numero += 1
+
+#######################################################################################
+
+#contador de palabras
+
+oracion = input("ingrese una oracion: ")
+
+palabras = oracion.split()
+cantidad_palabras = len(palabras)
+
+print("la oracion ingresada tiene", cantidad_palabras, "palabra(s)")
+
+########################################################################################
+
+#conversor de temperatura
+
+######conversor de °C a °F:######
+temperatura_celsius = float(input("ingrese la temperatura en grados celsius: "))
+
+#formula segun el chatGPT para convertir: (0°C × 9/5) + 32 = 32°F
+temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32
+
+print("la temperatura en grados fahrenheit es:", temperatura_fahrenheit)
+
+######conversor de °F a °C:######
+temperatura_fahrenheit = float(input("ingresar la temperatura en grados fahrenheit: "))
+
+#formula: (0*F - 32) / 1,8
+temperatura_celsius = (temperatura_fahrenheit - 32) / 1.8
+print("la temperatura en grados celsius es:", temperatura_celsius)
+
+########################################################################################
+
+#adivina el numero
+import random
+
+numero_adivinar = random.randint(1,100)
+
+while True:
+    numero_ingresado = int(input("ingrese un numero: "))
+
+    if numero_ingresado == numero_adivinar :
+        print("adivinasteee!")
+        break
+    elif numero_ingresado < numero_adivinar  :
+        print("es chico")
+
+    else:
+        print("es grande")
+
+###########################################################################################
+
