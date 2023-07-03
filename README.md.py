34245e984ea4a0e00b8c39e244343491a1ab03d0
Sabrina240597 <sabrin.bonomo@gmail.com>
2023-07-03 18:50:43 -0300

    agregando la calculadora

--- README.md.py
+++ README.md.py
@@ -10,3 +10,28 @@ suma = int(numero1) + int(numero2)
 
 #Imprimir la suma
 print("la suma de", numero1 ,"y", numero2 , "es", suma )
+
+######################################################################
+
+#creando una calculadora
+
+numero1 = input("ingrese su primer numero...")
+
+operacion= input("ingrese la operacion deseada...")
+
+numero2 = input("ingrese su segundo numero...")
+
+#Realizar la operación correspondiente
+if operacion == '+':
+    resultado = int(numero1) + int(numero2)
+elif operacion == '-':
+    resultado =int(numero1) - int(numero2)
+elif operacion == '*':
+    resultado = int(numero1) *int(numero2)
+elif operacion == '/':
+    resultado =int(numero1) / int(numero2)
+else:
+    resultado = "Operación inválida"
+
+print("su resultado es...", resultado)
+#####################################################################
