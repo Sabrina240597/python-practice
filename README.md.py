d9515fb64cc884291dedcb184eb9ebf98abd7a72
Sabrina240597 <sabrin.bonomo@gmail.com>
2023-07-03 18:24:31 -0300

    primeras practicas

---
+++ README.md.py
@@ -0,0 +1,12 @@
+#como se hace una suma o otro tipo de calculo:
+
+#Solicitar al usuario dos números enteros
+numero1 = input("ingrese el primer numero...")
+
+numero2 = input("ingrese el segundo numero...")
+
+#Calcular a suma (o otro tipo de calculo) de los dos números
+suma = int(numero1) + int(numero2)
+
+#Imprimir la suma
+print("la suma de", numero1 ,"y", numero2 , "es", suma )
